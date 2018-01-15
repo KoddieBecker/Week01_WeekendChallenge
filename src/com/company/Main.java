654c75f06f8c5151e747d2ad8e10b6ab1b76b1f0
Koddie Becker <koddiebecker@Koddies-MacBook-Pro-5.local>
2018-01-15 16:09:07 -0700

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    Boolean ask = true;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Input your number");
+        String charecterNumber1 = sc.nextLine();
+        int realNumber1 = Integer.parseInt(charecterNumber1);
+
+        System.out.println(" Enter your operator: +, -, /, * ");
+        String operator = sc.nextLine();
+
+        System.out.println("Input 2nd number");
+        String charecterNumber2 = sc.nextLine();
+        int realNumber2 = Integer.parseInt(charecterNumber2);
+
+        int realNumber3 = 0;
+        //String charecterNumber3 = sc.nextLine();
+
+        if (operator.equals("+")) {
+            realNumber3 = (realNumber1 + realNumber2);
+
+        } else if (operator.equals("/")) {
+            realNumber3 = (realNumber1 / realNumber2);
+
+        } else if (operator.equals("*")) {
+            realNumber3 = (realNumber1 * realNumber2);
+
+
+        } else if (operator.equals("-")) {
+            realNumber3 = (realNumber1 - realNumber2);
+        }
+
+            System.out.println("Final results" + realNumber3);
+
+
+            System.out.println("Do you want another calculation?");
+
+            if ask = true
+
+
+
+
+        }
+    }
+
